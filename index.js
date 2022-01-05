f0a2513ed4ca766470bb6d7c150c326cedcab18c
DTGlov <67828079+JaeGlo@users.noreply.github.com>
2022-01-05 11:32:47 +0000

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const { addition, multiply, modulus } = require('./operations');
+const { addition, multiply, modulus, findLCM, findHCF } = require('./operations');
 const { fibo } = require('./fibonacci');
 
 const app = express();
@@ -24,8 +24,17 @@ app.post('/modulus', (req, res) => {
     return res.json(modulus(req.body.num1, req.body.num2));
 });
 
+app.post('/lcm', (req, res) => {
+    return res.json(findLCM(req.body.num1, req.body.num2));
+});
+
+app.post('/hcf', (req, res) => {
+    return res.json(findHCF(req.body.num1, req.body.num2));
+});
+
 app.post('/fib', (req, res) => {
     return res.json(fibo(req.body.number));
 });
+
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
--- operations.js
+++ operations.js
@@ -10,8 +10,35 @@ function modulus(a, b) {
     return a % b;
 }
 
+function findLCM(num1, num2) {
+    let min = (num1 > num2) ? num1 : num2;
+    // while loop
+    while (true) {
+        if (min % num1 == 0 && min % num2 == 0) {
+            return min;
+        }
+        min++;
+    }
+}
+
+function findHCF(number1, number2) {
+    let hcf;
+    // looping from 1 to number1 and number2
+    for (let i = 1; i <= number1 && i <= number2; i++) {
+
+        // check if is factor of both integers
+        if (number1 % i == 0 && number2 % i == 0) {
+            hcf = i;
+        }
+    }
+    return hcf;
+
+}
+
 module.exports = {
     addition,
     multiply,
-    modulus
+    modulus,
+    findLCM,
+    findHCF
 };
